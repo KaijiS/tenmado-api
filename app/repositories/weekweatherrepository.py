0c626d3a3aaa5e337276fa260d6834577e4dc15e
杉本階嗣 <>
2021-12-24 02:51:35 +0900

    doc: firestoreメモ

--- app/repositories/weekweatherrepository.py
+++ app/repositories/weekweatherrepository.py
@@ -11,6 +11,12 @@ def findbylargeareacode(
     report_date_to: datetime.date,
     report_days: int,
 ):
+    # firestoreメモ
+    # Reference 概念
+    # Snapshot 実態
+
+    # 「collecion reference」(db.collection)に対してwhereやorder系を実施すると 「query」という概念を取得(クエリ命令のみ)
+    # それに対しstream()を実行するとクエリが実行され、実態である「query document snapshot」を要素とする配列を取得する
     weekweather_querydocumentsnapshots = (
         db.collection("weekweather")
         .where("large_area_code", "==", large_area_code)
@@ -30,9 +36,11 @@ def findbylargeareacode(
 
     weekweathers = []
     for weekweather_querydocumentsnapshot in weekweather_querydocumentsnapshots:
+        # snapshot(実態を辞書に)
         weekweather = weekweather_querydocumentsnapshot.to_dict()
 
         forecast_querydocumentsnapshots = (
+            # snapshotのreferenceに対してcollection(subcollection)を指定して上述同様にqueryの実行
             weekweather_querydocumentsnapshot.reference.collection("forecasts")
             .order_by("forecast_target_date")
             .limit(report_days)
